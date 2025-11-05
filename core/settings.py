9a76d1172190056ea814b2455f26542ddde3db65
jmosquera <jmosquera2305@gmail.com>
2025-11-05 16:29:58 +0100

    fix: simplify static files storage config

--- core/settings.py
+++ core/settings.py
@@ -167,9 +167,11 @@ STORAGES = {
         'BACKEND': 'cloudinary_storage.storage.MediaCloudinaryStorage',
     },
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
     },
 }
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # Default primary key field type
